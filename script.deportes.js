ec7337c8d529722f200713153a7866e51077e271
Thomas <thomas.agustin.vidal@gmail.com>
2023-08-20 11:15:54 -0300

    fetch

---
+++ script.deportes.js
@@ -0,0 +1,21 @@
+let deportes = document.querySelector("#deportes");
+
+fetch("data.json")
+.then((resp) => resp.json())
+.then((data) => {
+    console.log(data);
+
+    data.map((item) => {
+    let content = document.createElement("div");
+    content.innerHTML = `
+        <h4>${item.name}</h4>
+        <img src="${item.img}" alt="${item.name}">
+        <p>${item.description}</p>
+    `;
+    deportes.append(content);
+    });
+})
+.catch((error) => {
+    console.error("Error al cargar los datos:", error);
+});
+
